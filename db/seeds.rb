1c9fdfae9a8f30ac008c130f2632aaf26d20ccbd
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-25 13:29:55 -0400

    Test model relationships to be working in tux. Populate seeds.rb, except for password authentication for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-5.times do |i|
-  User.create(username: "user#{i}", email: "email#{i}", password_digest: "pass#{i}")
-end
+
+user1 = User.create(username: "Robert Browning", email: "rb@rb.com", password_digest: "bells_and_pomegranates")
+user2 = User.create(username: "Elizabeth Barrett Browning", email: "ebb@ebb.com", password_digest: "aurora_leigh")
 
 5.times do |i|
   Post.create(content: "this is my content#{i}")
@@ -10,14 +10,19 @@ Board.create(topic: "Victorian Poetry")
 Board.create(topic: "German Romanticism")
 
 Post.all.each_with_index do |post, index|
-  user = User.all.select {|user| user.id == post.id }
-  post.user = user
-  user.posts << post
   if index <= 3
+    user = User.first
+    post.user = user
+    user.posts << post
+
     board = Board.find_by(topic: "Victorian Poetry")
     post.board = board
     board.posts << post
   else
+    user = User.last
+    post.user = user
+    user.posts << post
+
     board = Board.find_by(topic: "German Romanticism")
     post.board = board
     board.posts << post
