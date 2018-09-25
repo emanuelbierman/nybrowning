084a9826c1238b23d9616f9f817f5fd5e0c0e9ba
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-25 14:43:32 -0400

    Modify seeds.rb. Confirm working model relationships.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
 
-user1 = User.create(username: "Robert Browning", email: "rb@rb.com", password_digest: "bells_and_pomegranates")
-user2 = User.create(username: "Elizabeth Barrett Browning", email: "ebb@ebb.com", password_digest: "aurora_leigh")
+user1 = User.new(username: "Robert Browning", email: "rb@rb.com", password: "sordello", password_confirmation: "sordello")
+user2 = User.new(username: "Elizabeth Barrett Browning", email: "ebb@ebb.com", password: "aurora", password_confirmation: "aurora")
+user1.save
+user2.save
 
 5.times do |i|
-  Post.create(content: "this is my content#{i}")
+  post = Post.create(content: "this is his content#{i}")
+  post.user = User.first
+  User.first.posts << post
+end
+
+5.times do |i|
+  post = Post.create(content: "this is her content#{i}")
+  post.user = User.last
+  User.last.posts << post
 end
 
 Board.create(topic: "Victorian Poetry")
 Board.create(topic: "German Romanticism")
 
 Post.all.each_with_index do |post, index|
-  if index <= 3
-    user = User.first
-    post.user = user
-    user.posts << post
-
+  if index <= 4
     board = Board.find_by(topic: "Victorian Poetry")
     post.board = board
     board.posts << post
   else
-    user = User.last
-    post.user = user
-    user.posts << post
-
     board = Board.find_by(topic: "German Romanticism")
     post.board = board
     board.posts << post
