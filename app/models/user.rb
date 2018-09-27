ae8a2b31969046398037e6cb65059d7598664098
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-27 13:03:39 -0400

    Add and successfully test User.slug and  User.find_by_slug. Add #logged_in? and #current_user but cannot confirm working yet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email, :password
   has_many :posts
   has_many :boards, through: :posts
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|user| user.slug == slug }
+  end
 end
