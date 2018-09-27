56c2f21edaa962dd5f81df7efd1c7856e52f3d56
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-27 14:10:13 -0400

    Build routes for Users. Make notes inside all existing views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,49 @@
 class UsersController < ApplicationController
 
+  get '/users' do
+    # users can't see all users
+  end
+
+  get '/users/login' do
+    # users can log in or go to the board index
+    if logged_in?
+      erb :'/boards'
+    else
+      erb :'users/login_signup'
+    end
+  end
+
+  get '/users/new' do
+    # users can sign up
+    redirect to '/users/login'
+  end
+
+  get '/users/:id' do
+    # users can only see their own profile, and no one else's
+    @user = User.find(params[:user_id])
+    if logged_in? && current_user == @user
+      erb :'users/show'
+    else
+      redirect to '/boards'
+    end
+  end
+
+  patch '/users/:id' do
+    # users can update their own information
+    @user = User.find(params[:user_id])
+  end
+
+  post '/users/new' do
+    # after a user signs up, they go to the boards index
+    @user = User.create(params[:user])
+    redirect to '/boards'
+  end
+
+  delete '/users/:id' do
+    # users can delete their own account, and no one else's
+    @user = User.find(params[:user_id])
+  end
+
   def logged_in?
     !!session[:user_id]
   end
